5f5fb062c4666863b2ab26752df6a2c6e7793265
Devjabli <aroussiotmane27@gmail.com>
2024-06-29 01:39:23 +0100

    update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install a specific version of flask (2.1.0)
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
