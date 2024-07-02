046387695b625caeb37842d03fd9020e835c7f38
Devjabli <aroussiotmane27@gmail.com>
2024-07-02 22:44:15 +0100

    task 0 1 3 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Puppet for project automation
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/Ajoe62 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
