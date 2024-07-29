abf5da667a0b449a884e69673a2de8efba0b23ae
Devjabli <aroussiotmane27@gmail.com>
2024-07-30 00:21:27 +0100

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Access REST API to todo lists of employees"""
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    empId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + empId
+    
+    res = requests.get(url)
+    empName = res.json().get('name')
+    
+    todoUrl = url + "/todos"
+    res = requests.get(todoUrl)
+    tasks = res.json()
+    done = 0
+    com_tasks = []
+    
+    for task in tasks:
+        if task.get('completed'):
+            com_tasks.append(task)
+            done += 1
+    
+    print("Employee {} is done with tasks({}/{}):".
+          format(empName, done, len(tasks)))
+    
+    for task in com_tasks:
+        print("\t {}".format(task.get('title')))
