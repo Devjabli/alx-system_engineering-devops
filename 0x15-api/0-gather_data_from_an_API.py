71744535c0b02679696b89dd4512e99028dbb6e3
Devjabli <aroussiotmane27@gmail.com>
2024-07-30 00:26:55 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
+"""Access REST API to todo lists of employees"""
 
-import requests
 import sys
-
+import requests
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
+    empId = sys.argv[1]
     baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
-
-    response = requests.get(url)
-    employeeName = response.json().get('name')
-
+    url = baseUrl + "/" + empId
+    
+    res = requests.get(url, timeout=10)
+    empName = res.json().get('name')
+    
     todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
+    res = requests.get(todoUrl, timeout=10)
+    tasks = res.json()
     done = 0
-    done_tasks = []
-
+    com_tasks = []
+    
     for task in tasks:
         if task.get('completed'):
-            done_tasks.append(task)
+            com_tasks.append(task)
             done += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
-
-    for task in done_tasks:
+    
+    print("Employee {} is done with tasks({}/{}):".
+          format(empName, done, len(tasks)))
+    
+    for task in com_tasks:
         print("\t {}".format(task.get('title')))
