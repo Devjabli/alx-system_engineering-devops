933a4ac5230958a26af6b756cae0d613c2a05d23
Devjabli <aroussiotmane27@gmail.com>
2024-07-30 00:28:58 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,23 +8,23 @@ if __name__ == '__main__':
     empId = sys.argv[1]
     baseUrl = "https://jsonplaceholder.typicode.com/users"
     url = baseUrl + "/" + empId
-    
+
     res = requests.get(url, timeout=10)
     empName = res.json().get('name')
-    
+
     todoUrl = url + "/todos"
     res = requests.get(todoUrl, timeout=10)
     tasks = res.json()
     done = 0
     com_tasks = []
-    
+
     for task in tasks:
         if task.get('completed'):
             com_tasks.append(task)
             done += 1
-    
+
     print("Employee {} is done with tasks({}/{}):".
           format(empName, done, len(tasks)))
-    
+
     for task in com_tasks:
         print("\t {}".format(task.get('title')))
