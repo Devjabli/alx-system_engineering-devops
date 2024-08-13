89496102f41bbfaef60ffb7dbe8614289e7f4928
Devjabli <aroussiotmane27@gmail.com>
2024-08-14 00:37:45 +0100

    task 0 updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,5 +15,4 @@ def number_of_subscribers(subreddit):
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
-        return 0
+    return 0
