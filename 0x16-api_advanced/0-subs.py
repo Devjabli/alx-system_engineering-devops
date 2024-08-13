ce8988f1a9da9fd329821431cb3d65abec910f02
Devjabli <aroussiotmane27@gmail.com>
2024-08-14 00:24:20 +0100

    task 1 updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False, timeout=30)
     if response.status_code == 404:
         return 0
     data = response.json().get("data")
