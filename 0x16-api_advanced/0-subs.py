7f1e1006106e8da9ba55a7c3c93aa23b3cb0182d
Devjabli <aroussiotmane27@gmail.com>
2024-08-14 00:47:51 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""Function"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False, timeout=20)
-    if response.status_code == 404:
+    """
+    Return the total number of subscribers on a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "subreddit-subscriber-counter/0.1"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False, timeout=20)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    except requests.RequestException:
+        return 0
